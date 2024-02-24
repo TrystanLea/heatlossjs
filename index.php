d218e00860fe399148eaaf8d6bf2d06a4ab69ec0
TrystanLea <trystan.lea@gmail.com>
2024-02-24 23:21:18 +0000

    updated example

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
     $v = time(); 
-    $example = file_get_contents("files/midterrace.json");
+    $example = file_get_contents("files/midterrace.json?v=2");
 
 ?>
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
